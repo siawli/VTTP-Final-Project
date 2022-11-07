191372fdb9dd82c3e8f302679ddc8415031a72a6
TSL <TSL@Siaws-MacBook-Air.local>
2022-11-07 09:51:49 +0800

    yaml file

--- FPServer/src/main/java/VTTP/FinalProject/configurations/AmazonS3Config.java
+++ FPServer/src/main/java/VTTP/FinalProject/configurations/AmazonS3Config.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
-import com.amazonaws.partitions.model.Endpoint;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
--- FPServer/src/main/java/VTTP/FinalProject/configurations/RedisConfiguration.java
+++ FPServer/src/main/java/VTTP/FinalProject/configurations/RedisConfiguration.java
@@ -1,12 +1,10 @@
 package VTTP.FinalProject.configurations;
 
 import java.time.Duration;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
