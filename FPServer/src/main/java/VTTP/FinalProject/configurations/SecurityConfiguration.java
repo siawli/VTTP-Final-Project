0881d68c185a03fa614e5bf879899ec47821d312
TSL <TSL@Siaws-MacBook-Air.local>
2022-11-08 09:24:26 +0800

    jwt token url exclusion; deployment

--- FPServer/src/main/java/VTTP/FinalProject/configurations/SecurityConfiguration.java
+++ FPServer/src/main/java/VTTP/FinalProject/configurations/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration {
         .disable()
         .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
         .authorizeRequests()
-        .antMatchers("/assets/**", "/polyfills.b9005cfd1a54175e.js", "/main.7f8d6fecb80ce682.js", "/styles.43aecdfb5c9c28ca.css", "/runtime.e3f5fe3eca1bd5d1.js", "/favicon.ico", "/", "/login", "/signUp","/authenticate", "/signUp") // routes indicated here is not protected
+        .antMatchers("/authenticate", "/signUp") // routes indicated here is not protected
         .permitAll()
         .anyRequest()
         .authenticated()
@@ -49,6 +49,10 @@ public class SecurityConfiguration {
         // http.authenticationProvider(authenticationProvider());
 
         return http.build();
+
+        /*
+         "/assets/**", "/polyfills.b9005cfd1a54175e.js", "/main.7f8d6fecb80ce682.js", "/styles.43aecdfb5c9c28ca.css", "/runtime.e3f5fe3eca1bd5d1.js", "/favicon.ico", "/", "/login", "/signUp",
+         */
     }
 
 
