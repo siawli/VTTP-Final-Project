30d33fed6396963671931be233b144774314d13d
TSL <TSL@Siaws-MacBook-Air.local>
2022-11-12 23:36:02 +0800

    final

--- FPServer/src/main/java/VTTP/FinalProject/configurations/SecurityConfiguration.java
+++ FPServer/src/main/java/VTTP/FinalProject/configurations/SecurityConfiguration.java
@@ -51,7 +51,6 @@ public class SecurityConfiguration {
 
     }
 
-
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
